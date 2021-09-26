557488048f6ff1b58bd1acb2e056ca5525bd30c3
Progyan Bhattacharya <bprogyan@gmail.com>
2021-09-27 02:26:47 +0530

    jest-monaco

--- __tests__/monaco/jest.config.js
+++ __tests__/monaco/jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     ...baseConfig.moduleNameMapper,
+    "@otjs/monaco/(.*)": "<rootDir>/packages/monaco/src/$1",
     "monaco-editor":
       "<rootDir>/node_modules/monaco-editor/esm/vs/editor/editor.api.js",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
