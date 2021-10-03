5f406f70043fc499dc2c771df6fc4910b75da94c
Progyan Bhattacharya <bprogyan@gmail.com>
2021-10-03 11:21:39 +0530

    (fix) Monaco Adapter: Cleanup ClassName Logic for Remote Cursor
    Turn ',' character into '_' and remove rest of the non-alphanumeric characters from the color string to create classname of the remote cursor/selection.
    
    Fixes: #61
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

--- __tests__/monaco/__tests__/monaco-adapter.spec.ts
+++ __tests__/monaco/__tests__/monaco-adapter.spec.ts
@@ -250,7 +250,7 @@ describe("Test Monaco Adapter", () => {
         expect.arrayContaining([
           expect.objectContaining({
             options: expect.objectContaining({
-              className: "remote-client-_09c-cursor",
+              className: "remote-client-09c-cursor",
               isWholeLine: false,
               stickiness:
                 monaco.editor.TrackedRangeStickiness
@@ -281,7 +281,7 @@ describe("Test Monaco Adapter", () => {
         expect.arrayContaining([
           expect.objectContaining({
             options: expect.objectContaining({
-              className: "remote-client-_0ff-selection",
+              className: "remote-client-0ff-selection",
               isWholeLine: false,
               stickiness:
                 monaco.editor.TrackedRangeStickiness
@@ -314,7 +314,7 @@ describe("Test Monaco Adapter", () => {
         expect.arrayContaining([
           expect.not.objectContaining({
             options: expect.objectContaining({
-              className: "remote-client-_0ee-cursor",
+              className: "remote-client-0ee-cursor",
               isWholeLine: false,
               stickiness:
                 monaco.editor.TrackedRangeStickiness
--- packages/monaco/src/monaco-adapter.ts
+++ packages/monaco/src/monaco-adapter.ts
@@ -311,7 +311,9 @@ export class MonacoAdapter implements IEditorAdapter {
     }
 
     /** Remove non-alphanumeric characters to create valid classname */
-    const cursorColorTitle = cursorColor.replace(/\W+/g, "_");
+    const cursorColorTitle = cursorColor
+      .replaceAll(",", "_")
+      .replace(/\W+/g, "");
     const className = `remote-client-${cursorColorTitle}`;
 
     /** Generate Style rules and add them to document */
