8c5d8c62e016523be0247ab9fdd84e2cb651fec4
Progyan Bhattacharya <bprogyan@gmail.com>
2021-11-04 11:14:26 +0530

    precommit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  "{package.json, yarn.lock}": ["check-yarn-lock"],
+  "package.json": ["sort-package-json", "check-yarn-lock"],
+  "yarn.lock": ["check-yarn-lock"],
   "*.{js,ts,json,md}": ["prettier --write"],
 };
