a0fcff02e5c2e188c6d3e8be28bd1a0ef995fb21
Progyan Bhattacharya <bprogyan@gmail.com>
2022-12-18 12:03:47 +0530

    Fix Monaco CI issue x3
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

--- __tests__/monaco/jest.config.js
+++ __tests__/monaco/jest.config.js
@@ -27,7 +27,7 @@ module.exports = {
   testMatch: [`<rootDir>/${package.rootDir}/__tests__/*.[jt]s?(x)`],
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.([jt]sx?)$": "babel-jest",
+    "^.+\\.(jsx?)$": "babel-jest",
   },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ["node_modules/(?!(monaco-editor)/)"],
