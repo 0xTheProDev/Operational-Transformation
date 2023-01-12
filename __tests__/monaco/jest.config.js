60fb0192934debf48630ab6106ef4ea496a73161
Progyan Bhattacharya <bprogyan@gmail.com>
2023-01-12 20:55:14 +0530

    Stupid Me

--- __tests__/monaco/jest.config.js
+++ __tests__/monaco/jest.config.js
@@ -27,7 +27,7 @@ module.exports = {
   testMatch: [`<rootDir>/${package.rootDir}/__tests__/*.[jt]s?(x)`],
   // A map from regular expressions to paths to transformers
   transform: {
-    "\\.jsx?$": "babel-jest",
+    "<rootDir>/node_modules/monaco-editor/.+\\.(j|t)sx?$": "ts-jest",
   },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ["<rootDir>/node_modules/(?!(monaco-editor)/)"],
